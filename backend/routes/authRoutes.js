3382900011c3e4b86b32ee26f86928b793c7812e
Thamothara N <thamothara2017@gmail.com>
2026-03-09 23:22:06 +0530

    Migrate Vite to Next.js, fix hydration bugs and Tailwind v4 integration

--- backend/routes/authRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { authAdmin, setupInitialAdmin } = require('../controllers/authController');
-
-router.post('/login', authAdmin);
-router.post('/setup', setupInitialAdmin);
-
-module.exports = router;
--- backend/routes/packageRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getPackages, getPackageById, createPackage, updatePackage, deletePackage } = require('../controllers/packageController');
-
-// All routes are open (admin auth is handled on frontend side)
-router.route('/').get(getPackages).post(createPackage);
-router.route('/:id').get(getPackageById).put(updatePackage).delete(deletePackage);
-
-module.exports = router;
